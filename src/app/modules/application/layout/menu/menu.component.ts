fa65e7925c8fdd37bb6fd1badef755aa8354fdf1
adrianhfoster <adrian@opencollab.co.za>
2021-11-03 15:14:10 +0200

    UWCSI-107 PDFM - Generic color usage

--- src/app/modules/application/layout/menu/menu.component.ts
+++ src/app/modules/application/layout/menu/menu.component.ts
@@ -1,5 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
 import {RoutesEnum} from "@coreModule/utils/routes.enum";
+import {SettingsService} from "@pdfMarkerModule/services/settings.service";
+import {AppService} from "@coreModule/services/app.service";
 
 @Component({
   selector: 'pdf-marker-menu',
@@ -8,6 +10,9 @@ import {RoutesEnum} from "@coreModule/utils/routes.enum";
 })
 export class MenuComponent implements OnInit {
   readonly settings: string = "Settings";
+
+  primaryColour: string;
+  defaultColour: string = 'rgba(10,26,92,0.8)';
 
   readonly toolbarMenu = [
     { id: "Home",  toolTip: "Home", icon: "home", href: RoutesEnum.MARKER},
@@ -22,7 +27,21 @@ export class MenuComponent implements OnInit {
     { id: "comments",  title: "Generic Comments", icon: "comment_bank", href: RoutesEnum.ASSIGNMENT_COMMENTS },
     { id: "workingFolder",  title: "Working Folders", icon: "folder", href: RoutesEnum.ASSIGNMENT_WORKING_FOLDER }
   ];
-  constructor() { }
+
+  constructor(private settingsService: SettingsService,
+              private appService: AppService,
+              private elementRef: ElementRef) {
+    this.settingsService.getConfigurations().subscribe(configurations => {
+      if (configurations.defaultColour) {
+        this.elementRef.nativeElement.style.setProperty('--pdf-marker-primary', configurations.defaultColour);
+        this.primaryColour = configurations.defaultColour;
+      }else{
+        this.primaryColour = this.defaultColour;
+      }
+    }, error => {
+      this.appService.isLoading$.next(false);
+    });
+  }
 
   ngOnInit() {
   }
